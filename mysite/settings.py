8e47b50d5394fe0cb2af86013913fe27d3c44b96
Eashan Panjwani <eashan.panjwani1@gmail.com>
2020-04-22 13:38:07 -0400

    this dumb

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,6 @@ ALLOWED_HOSTS = ['module7-464898-465051.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'mathfilters',
     'leagues.apps.LeaguesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mathfilters',
 ]
 
 MIDDLEWARE = [
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'US/Eastern'
 
 USE_I18N = True
 
