92c6cab5c4750894f1b618dfba42dd7b36263b9f
Eashan Panjwani <eashan.panjwani1@gmail.com>
2020-04-23 12:44:15 -0400

    jlkjk

--- leagues/urls.py
+++ leagues/urls.py
@@ -9,4 +9,7 @@ path('login', views.login, name='login'),
 path('logout', views.logout, name='logout'),
 path('createleague', views.createLeague, name ='createleague'),
 path('enterteams', views.enterteams, name='enterteams'),
-path('enterdata1', views.enterdata1, name='enterdata1')]
+path('enterdata1', views.enterdata1, name='enterdata1'),
+path('generatebracket', views.generatebracket, name='generatebracket'),
+path('generatebracket2', views.generatebracket2, name='generatebracket2'),
+]
